a86d6e8b14b576c9ef11b41f6d8404668edfeb61
gnf22 <gnferreira2000@gmail.com>
2021-03-06 15:51:37 -0300

    Create AppError Class

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata';
 import '../../utils/module-alias';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
+import { AppError } from '@shared/errors/AppError';
 import { routes } from './routes';
 
 import '@shared/infra/typeorm';
@@ -12,5 +13,21 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => console.log('Server started at port 3333'));
