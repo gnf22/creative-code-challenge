3e9677836959f6122c2bbd39a004ec80fa13140e
gnf22 <gnferreira2000@gmail.com>
2021-03-06 20:14:58 -0300

    Create address migration and entitie.

---
+++ src/modules/addresses/infra/typeorm/entities/Address.ts
@@ -0,0 +1,56 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  JoinColumn,
+  OneToMany,
+} from 'typeorm';
+
+import { User } from '@modules/users/infra/typeorm/entities/User';
+
+import { v4 as uuid } from 'uuid';
+
+@Entity('addresses')
+export class Address {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  user_id: string;
+
+  @OneToMany(() => User, user => user.id)
+  @JoinColumn({ name: 'user_id' })
+  user: User[];
+
+  @Column()
+  address: string;
+
+  @Column()
+  number: number;
+
+  @Column()
+  complement: string;
+
+  @Column()
+  cep: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  state: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
---
+++ src/shared/infra/typeorm/migrations/1615070949051-CreateAddress.ts
@@ -0,0 +1,71 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateAddress1615070949051 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'addresses',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'address',
+            type: 'varchar',
+          },
+          {
+            name: 'number',
+            type: 'integer',
+          },
+          {
+            name: 'complement',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'cep',
+            type: 'varchar',
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+          },
+          {
+            name: 'state',
+            type: 'varchar',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'AddressUser',
+            columnNames: ['user_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'users',
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('addresses');
+  }
+}
